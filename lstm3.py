83cdb9fa5d7eb7726cdccc038eae0f609a766c94
magicpieh28 <choi.jungwon.ca1@gmail.com>
2018-09-18 21:48:35 +0900

    just consideration(#) of the RuntimeError

--- lstm3.py
+++ lstm3.py
@@ -50,13 +50,24 @@ class LSTMModel(nn.Module):
 		self.hidden_dim = hidden_dim
 
 		self.char_embedding = nn.Embedding(charset_size, c_embedding_dim)
+		# 17 * 32
 		self.lstm1 = nn.LSTM(c_embedding_dim, c_hidden_dim)
+		# 32 * 32
 
 		self.c_hx = nn.Parameter(torch.zeros(1, 1, self.c_hidden_dim))
 		self.c_cx = nn.Parameter(torch.zeros(1, 1, self.c_hidden_dim))
 
 		self.word_embedding = nn.Embedding(vocab_size, w_embedding_dim)
+		# 5 * 32
 		self.lstm2 = nn.LSTM(w_embedding_dim + c_hidden_dim, hidden_dim)
+		# (32 + 32) * 32
+		# w_embedding_dim + c_hidden_dim은 32 + 32가 되어야 하는데 (그래서 64 에러)
+		# char_lstm + w_embeds 를 0으로 torch.cat()하면 5 + 17이 되기 때문에 여전히 32
+		# 그러면 torch.cat()할 때 0이 아니라 1로 해야 된다는 건가? 32쪽으로 더해야 하나?
+		# 근데 1로는 더할수가 없는데 (5, 17이 안맞아서) 이걸 전치해서 더하면
+		# torch.Size([32, 1, 17]) + torch.Size([32, 1, 5])라는건데 이건 더해서 전치하는 것과
+		# 같지 않나? 결국에는 self.lstm2() 의 사이즈와는 크기의 위치?가 맞지 않아서 또 계산할 수 없음
+		# 근데 튜토리얼 힌트에 w_embed랑 c_rep의 차원을 더한 것을 lstm2 인풋값으로 해야 한다고..
 		self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
 
 		self.hx = nn.Parameter(torch.zeros(1, 1, hidden_dim))
@@ -66,14 +77,21 @@ class LSTMModel(nn.Module):
 
 	def forward(self, w_seq, c_seq):
 		c_embeds = self.char_embedding(c_seq)
+		# torch.Size([17, 32])
 		char_lstm, _ = self.lstm1(c_embeds.view(len(c_seq), 1, -1), (self.c_hx, self.c_cx))
+
+		# torch.Size([17, 1, 32])
 
 		w_embeds = self.word_embedding(w_seq)
 		w_3d_embeds = w_embeds.view(len(w_seq), 1, -1)
+		# torch.Size([5, 1, 32])
 
-		# print(torch.cat((char_lstm, w_3d_embeds), 0))
+		# torch.cat((char_lstm, w_3d_embeds), 0)
+		# torch.Size([22, 1, 32])
 
 		lstm_out, _ = self.lstm2(torch.cat((char_lstm, w_3d_embeds), 0), (self.hx, self.cx))
+		# cat()은 됐는데 self.lstm2()이 안돌아간다. 입력 사이즈가 64여야 하는데 32밖에 없단다.
+
 		tag_space = self.hidden2tag(lstm_out.view(len(c_seq)+len(w_seq), -1))
 		tag_score = F.log_softmax(tag_space, dim=1)
 		return tag_score
